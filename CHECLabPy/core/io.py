Fixed bug in size calculation

CHECLabPy/core/io.py
@@ -476,7 +476,7 @@ class HDFStoreReader(ABC):
         if 'size' not in df.metadata:
             print("WARNING: This file is outdated, please re-generate it "
                   "using scripts/extract_dl1.py")
-            df_row = df.loc[df['row'] == df.metadata.n_rows/2]
+            df_row = df.loc[df['row'] == df.metadata['n_rows']/2]
             x = df_row['xpix'].values
             df.metadata['size'] = np.min(np.diff(np.sort(x)))
         return df