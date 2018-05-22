b5c20baa02987141c0d7d06e0853c4ad9f6ac9f3
Jason Watson <jason.jw@live.co.uk>
2018-05-22 19:07:06 +0100

    extract_dl1 now accepts a list of files

--- scripts/extract_dl1.py
+++ scripts/extract_dl1.py
@@ -18,8 +18,8 @@ def main():
                    'various parameters extracted from the waveforms')
     parser = argparse.ArgumentParser(description=description,
                                      formatter_class=Formatter)
-    parser.add_argument('-f', '--file', dest='input_path', action='store',
-                        required=True, help='path to the TIO r1 run file')
+    parser.add_argument('-f', '--files', dest='input_paths', nargs='+',
+                        help='path to the TIO r1 run files')
     parser.add_argument('-m', '--monitor', dest='monitor', action='store',
                         help='path to the monitor file (OPTIONAL)')
     parser.add_argument('-o', '--output', dest='output_path', action='store',
@@ -45,90 +45,94 @@ def main():
         config = {}
         config_string = ""
 
-    reader = ReaderR1(args.input_path, args.max_events)
-    n_events = reader.n_events
-    n_modules = reader.n_modules
-    n_pixels = reader.n_pixels
-    n_samples = reader.n_samples
-    n_cells = reader.n_cells
-    pixel_array = np.arange(n_pixels)
-    camera_version = reader.camera_version
-    mapping = reader.mapping
-    if 'reference_pulse_path' not in config:
-        config['reference_pulse_path'] = reader.reference_pulse_path
+    input_paths = args.input_paths
+    n_files = len(input_paths)
+    for i_path, input_path in enumerate(input_paths):
+        print("PROGRESS: Reducing file {}/{}".format(i_path + 1, n_files))
+        reader = ReaderR1(input_path, args.max_events)
+        n_events = reader.n_events
+        n_modules = reader.n_modules
+        n_pixels = reader.n_pixels
+        n_samples = reader.n_samples
+        n_cells = reader.n_cells
+        pixel_array = np.arange(n_pixels)
+        camera_version = reader.camera_version
+        mapping = reader.mapping
+        if 'reference_pulse_path' not in config:
+            config['reference_pulse_path'] = reader.reference_pulse_path
 
-    kwargs = dict(
-        n_pixels=n_pixels,
-        n_samples=n_samples,
-        plot=args.plot,
-        **config
-    )
-    reducer = WaveformReducerFactory.produce(args.reducer, **kwargs)
-    baseline_subtractor = BaselineSubtractor(reader)
-
-    input_path = reader.path
-    output_path = args.output_path
-    if not output_path:
-        output_path = input_path.rsplit('_r1', 1)[0] + "_dl1.h5"
-
-    with DL1Writer(output_path, n_events*n_pixels, args.monitor) as writer:
-        t_cpu = 0
-        start_time = 0
-        desc = "Processing events"
-        for waveforms in tqdm(reader, total=n_events, desc=desc):
-            iev = reader.index
-
-            t_tack = reader.current_tack
-            t_cpu_sec = reader.current_cpu_s
-            t_cpu_ns = reader.current_cpu_ns
-            t_cpu = pd.to_datetime(
-                np.int64(t_cpu_sec * 1E9) + np.int64(t_cpu_ns),
-                unit='ns'
-            )
-            fci = reader.first_cell_ids
-
-            if not start_time:
-                start_time = t_cpu
-
-            waveforms_bs = baseline_subtractor.subtract(waveforms)
-            bs = baseline_subtractor.baseline
-
-            params = reducer.process(waveforms_bs)
-
-            df_ev = pd.DataFrame(dict(
-                iev=iev,
-                pixel=pixel_array,
-                first_cell_id=fci,
-                t_cpu=t_cpu,
-                t_tack=t_tack,
-                baseline_subtracted=bs,
-                **params
-            ))
-            writer.append_event(df_ev)
-
-        sn_dict = {}
-        for tm in range(n_modules):
-            sn_dict['TM{:02d}_SN'.format(tm)] = reader.get_sn(tm)
-
-        metadata = dict(
-            source="CHECLabPy",
-            date_generated=pd.datetime.now(),
-            input_path=input_path,
-            n_events=n_events,
-            n_modules=n_modules,
+        kwargs = dict(
             n_pixels=n_pixels,
             n_samples=n_samples,
-            n_cells=n_cells,
-            start_time=start_time,
-            end_time=t_cpu,
-            camera_version=camera_version,
-            reducer=reducer.__class__.__name__,
-            configuration=config_string,
-            **sn_dict
+            plot=args.plot,
+            **config
         )
+        reducer = WaveformReducerFactory.produce(args.reducer, **kwargs)
+        baseline_subtractor = BaselineSubtractor(reader)
 
-        writer.add_metadata(**metadata)
-        writer.add_mapping(mapping)
+        input_path = reader.path
+        output_path = args.output_path
+        if not output_path:
+            output_path = input_path.rsplit('_r1', 1)[0] + "_dl1.h5"
+
+        with DL1Writer(output_path, n_events*n_pixels, args.monitor) as writer:
+            t_cpu = 0
+            start_time = 0
+            desc = "Processing events"
+            for waveforms in tqdm(reader, total=n_events, desc=desc):
+                iev = reader.index
+
+                t_tack = reader.current_tack
+                t_cpu_sec = reader.current_cpu_s
+                t_cpu_ns = reader.current_cpu_ns
+                t_cpu = pd.to_datetime(
+                    np.int64(t_cpu_sec * 1E9) + np.int64(t_cpu_ns),
+                    unit='ns'
+                )
+                fci = reader.first_cell_ids
+
+                if not start_time:
+                    start_time = t_cpu
+
+                waveforms_bs = baseline_subtractor.subtract(waveforms)
+                bs = baseline_subtractor.baseline
+
+                params = reducer.process(waveforms_bs)
+
+                df_ev = pd.DataFrame(dict(
+                    iev=iev,
+                    pixel=pixel_array,
+                    first_cell_id=fci,
+                    t_cpu=t_cpu,
+                    t_tack=t_tack,
+                    baseline_subtracted=bs,
+                    **params
+                ))
+                writer.append_event(df_ev)
+
+            sn_dict = {}
+            for tm in range(n_modules):
+                sn_dict['TM{:02d}_SN'.format(tm)] = reader.get_sn(tm)
+
+            metadata = dict(
+                source="CHECLabPy",
+                date_generated=pd.datetime.now(),
+                input_path=input_path,
+                n_events=n_events,
+                n_modules=n_modules,
+                n_pixels=n_pixels,
+                n_samples=n_samples,
+                n_cells=n_cells,
+                start_time=start_time,
+                end_time=t_cpu,
+                camera_version=camera_version,
+                reducer=reducer.__class__.__name__,
+                configuration=config_string,
+                **sn_dict
+            )
+
+            writer.add_metadata(**metadata)
+            writer.add_mapping(mapping)
 
 
 if __name__ == '__main__':
