b43dd39c21a6e53f6a6432df358b537c41213dd4
Jason Watson <jason.jw@live.co.uk>
2018-06-04 21:27:50 +0100

    Updated MonitorReader
    - Updated to use new monitor format
    - Handles obtaining the column from the correct table without the user
    needing to know the underlying structure
    - get_average obtains the average of a column for the run

--- CHECLabPy/core/io.py
+++ CHECLabPy/core/io.py
@@ -368,7 +368,6 @@ class MonitorWriter:
 
         metadata = dict(
             input_path=monitor_path,
-            n_events=imon,
             start_time=start_time,
             end_time=t_cpu,
             n_modules=self.n_modules,
@@ -418,12 +417,16 @@ class MonitorWriter:
     def _save_metadata(self):
         print("Saving monitor metadata to HDF5 file")
         self.store['monitor_metadata'] = pd.DataFrame()
-        store = self.store.get_storer('monitor_metadata')
-        store.attrs.metadata = self.metadata
+        storer = self.store.get_storer('monitor_metadata')
+        storer.attrs.metadata = self.metadata
 
     def finish(self):
         self._append_to_file()
-        self.add_metadata(n_bytes=self.n_bytes)
+        n_events = self.store.select_column("monitor_camera", "imon").max() + 1
+        self.add_metadata(
+            n_bytes=self.n_bytes,
+            n_events=n_events
+        )
         self._save_metadata()
 
     def _set_data_monitor_index(self, data_ev, imon):
@@ -467,20 +470,32 @@ class MonitorWriter:
                 self._set_data_monitor_index(data_ev, imon)
 
 
-class HDFStoreReader(ABC):
+class DL1Reader:
     """
-    Base class for reading from HDFStores
+    Reader for the HDF5 DL1 Files
     """
-    @abstractmethod
-    def __init__(self):
-        self.store = None
-        self.key = None
+    def __init__(self, path):
+        print("Opening HDF5 file: {}".format(path))
+        if not os.path.exists(path):
+            raise FileNotFoundError("File does not exist: {}".format(path))
+        self.store = pd.HDFStore(
+            path, mode='r', complevel=9, complib='blosc:blosclz'
+        )
+        self.path = path
+        self.key = 'data'
+        self._monitor = None
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.store.close()
+
+    def __getitem__(self, iev):
+        start = iev * self.n_pixels
+        stop = (iev + 1) * self.n_pixels
+        df = self.select(start=start, stop=stop)
+        return df
 
     @property
     def metadata(self):
@@ -526,6 +541,40 @@ class HDFStoreReader(ABC):
         version = self.mapping.metadata.version
         camera_config = CameraConfiguration(version)
         return camera_config.GetMapping(self.n_modules == 1)
+
+    @property
+    def monitor(self):
+        if not self._monitor:
+            if 'monitor_metadata' in self.store:
+                self._monitor = MonitorReader(self.store)
+            else:
+                msg = ("Monitor information has not been "
+                       "stored inside this DL1 file")
+                raise AttributeError(msg)
+        return self._monitor
+
+    @property
+    def n_pixels(self):
+        return self.metadata['n_pixels']
+
+    @property
+    def n_samples(self):
+        return self.metadata['n_samples']
+
+    @property
+    def version(self):
+        return self.metadata['camera_version']
+
+    @staticmethod
+    def is_compatible(filepath):
+        try:
+            kwargs = dict(mode='r', complevel=9, complib='blosc:blosclz')
+            with pd.HDFStore(filepath, **kwargs) as store:
+                if 'data' not in store:
+                    return False
+        except IOError:
+            return False
+        return True
 
     def get_sn(self, tm):
         if tm >= self.n_modules:
@@ -679,63 +728,6 @@ class HDFStoreReader(ABC):
         n_rows = n_events * self.metadata['n_pixels']
         return next(self.iterate_over_chunks(n_rows))
 
-
-class DL1Reader(HDFStoreReader):
-    """
-    Reader for the HDF5 DL1 Files
-    """
-    def __init__(self, path):
-        super().__init__()
-        print("Opening HDF5 file: {}".format(path))
-        if not os.path.exists(path):
-            raise FileNotFoundError("File does not exist: {}".format(path))
-        self.store = pd.HDFStore(
-            path, mode='r', complevel=9, complib='blosc:blosclz'
-        )
-        self.path = path
-        self.key = 'data'
-        self._monitor = None
-
-    def __getitem__(self, iev):
-        start = iev * self.n_pixels
-        stop = (iev + 1) * self.n_pixels
-        df = self.select(start=start, stop=stop)
-        return df
-
-    @property
-    def monitor(self):
-        if not self._monitor:
-            if 'monitor' in self.store:
-                self._monitor = MonitorReader(self.store)
-            else:
-                msg = ("Monitor information has not been "
-                       "stored inside this DL1 file")
-                raise AttributeError(msg)
-        return self._monitor
-
-    @property
-    def n_pixels(self):
-        return self.metadata['n_pixels']
-
-    @property
-    def n_samples(self):
-        return self.metadata['n_samples']
-
-    @property
-    def version(self):
-        return self.metadata['camera_version']
-
-    @staticmethod
-    def is_compatible(filepath):
-        try:
-            kwargs = dict(mode='r', complevel=9, complib='blosc:blosclz')
-            with pd.HDFStore(filepath, **kwargs) as store:
-                if 'data' not in store:
-                    return False
-        except IOError:
-            return False
-        return True
-
     def get_monitor_column(self, monitor_index, column_name):
         """
         Get a column from the monitor column corresponding to the
@@ -760,12 +752,226 @@ class DL1Reader(HDFStoreReader):
         return column
 
 
-class MonitorReader(HDFStoreReader):
+class MonitorReader:
     def __init__(self, store):
-        super().__init__()
-        self.key = 'monitor'
         self.store = store
 
-    def iterate_over_events(self):
-        for df in self.iterate_over_chunks(self.metadata['n_modules']):
-            yield df
+    @property
+    def metadata(self):
+        return self.store.get_storer("monitor_metadata").attrs.metadata
+
+    @property
+    def columns_camera(self):
+        storer = self.store.get_storer("monitor_camera")
+        return storer.attrs.non_index_axes[0][1]
+
+    @property
+    def columns_tm(self):
+        storer = self.store.get_storer("monitor_tm")
+        return storer.attrs.non_index_axes[0][1]
+
+    @property
+    def columns_pixel(self):
+        storer = self.store.get_storer("monitor_pixel")
+        return storer.attrs.non_index_axes[0][1]
+
+    @property
+    def n_bytes(self):
+        return self.metadata['n_bytes']
+
+    @property
+    def n_events(self):
+        return self.metadata['n_events']
+
+    @property
+    def n_modules(self):
+        return self.metadata['n_modules']
+
+    def _load_entire_table(self, key, force=False):
+        """
+        Load the entire DataFrame into memory
+
+        Parameters
+        ----------
+        force : bool
+            Force the loading of a DataFrame into memory even if it is of a
+            large size
+
+        Returns
+        -------
+        df : `pandas.DataFrame`
+
+        """
+        print("Loading entire DataFrame from HDF5 file")
+        if (self.n_bytes > 8E9) and not force:
+            raise MemoryError("DataFrame is larger than 8GB, "
+                              "set force=True to proceed with loading the "
+                              "entire DataFrame into memory")
+        if self.n_bytes > 8E9:
+            print("WARNING: DataFrame is larger than 8GB")
+        return self.store[key]
+
+    def load_entire_table_camera(self, force=False):
+        """
+        Load the entire DataFrame into memory (monitor_camera)
+
+        Parameters
+        ----------
+        force : bool
+            Force the loading of a DataFrame into memory even if it is of a
+            large size
+
+        Returns
+        -------
+        df : `pandas.DataFrame`
+
+        """
+        return self._load_entire_table("monitor_camera", force)
+
+    def load_entire_table_tm(self, force=False):
+        """
+        Load the entire DataFrame into memory (monitor_tm)
+
+        Parameters
+        ----------
+        force : bool
+            Force the loading of a DataFrame into memory even if it is of a
+            large size
+
+        Returns
+        -------
+        df : `pandas.DataFrame`
+
+        """
+        return self._load_entire_table("monitor_tm", force)
+
+    def load_entire_table_pixel(self, force=False):
+        """
+        Load the entire DataFrame into memory (monitor_pixel)
+
+        Parameters
+        ----------
+        force : bool
+            Force the loading of a DataFrame into memory even if it is of a
+            large size
+
+        Returns
+        -------
+        df : `pandas.DataFrame`
+
+        """
+        return self._load_entire_table("monitor_pixel", force)
+
+    def get_key_for_column(self, column):
+        """
+        Search the monitor dataframes to find which contains the requested
+        column
+
+        Parameters
+        ----------
+        column : str
+            The column you wish to obtain
+
+        Returns
+        -------
+        str
+
+        """
+        if column in self.columns_camera:
+            return "monitor_camera"
+        elif column in self.columns_tm:
+            return "monitor_tm"
+        elif column in self.columns_pixel:
+            return "monitor_pixel"
+        else:
+            msg = "Column does not exist in monitor dataframes"
+            raise KeyError(msg)
+
+    def select_column(self, column, key=None, **kwargs):
+        """
+        Use the pandas.HDFStore.select_column method to obtain a single
+        column
+
+        Parameters
+        ----------
+        column : str
+            The column you wish to obtain
+        key : str
+            Force function to look for column in this dataframe
+        kwargs
+            Arguments to pass to pandas.HDFStore.select
+
+        Returns
+        -------
+        `pandas.Series`
+
+        """
+        if not key:
+            key = self.get_key_for_column(column)
+        return self.store.select_column(key, column, **kwargs)
+
+    def select_columns(self, columns, key=None, **kwargs):
+        """
+        Use the pandas.HDFStore.select_column method to obtain a list of
+        columns as numpy arrays
+
+        Parameters
+        ----------
+        columns : list
+            A list of the columns you wish to obtain
+        key : str
+            Force function to look for columns in this dataframe
+        kwargs
+            Arguments to pass to pandas.HDFStore.select
+
+        Returns
+        -------
+        values : list
+            List of numpy arrays containing the values for all of the columns
+
+        """
+        values = []
+        for c in columns:
+            values.append(self.select_column(c, key, **kwargs))
+        return values
+
+    def select_column_as_array(self, column, key=None, **kwargs):
+        """
+        Return select_column transformed into a numpy array
+
+        Parameters
+        ----------
+        column : str
+            The column you wish to obtain
+        key : str
+            Force function to look for column in this dataframe
+        kwargs
+            Arguments to pass to pandas.HDFStore.select
+
+        Returns
+        -------
+        ndarray
+
+        """
+        column = self.select_column(column, key, **kwargs).values
+        n_idevice = column.size // self.n_events
+        return np.reshape(column, (self.n_events, n_idevice))
+
+    def get_average(self, column):
+        """
+        Obtain the average for a column
+
+        Parameters
+        ----------
+        column : str
+            The column you wish to obtain
+
+        Returns
+        -------
+        ndarray
+
+        """
+        key = self.get_key_for_column(column)
+        idevice, col_values = self.select_columns(['idevice', column], key)
+        df = pd.DataFrame({"idevice": idevice, column: col_values})
+        return df.groupby('idevice').mean()[column].values
