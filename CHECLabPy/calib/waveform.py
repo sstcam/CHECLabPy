37e005b5c8d79f2f0495fa0a2eede4021f28ca5d
watsonjj <jason.jw@live.co.uk>
2019-04-18 16:27:13 +0200

    Restore n_pixels and n_samples

--- CHECLabPy/calib/waveform.py
+++ CHECLabPy/calib/waveform.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class WaveformCalibrator:
-    def __init__(self, pedestal_path, sn_list=None):
+    def __init__(self, pedestal_path, n_pixels, n_samples, sn_list=None):
         if sn_list is None:
             from target_calib import CalibratorMultiFile
             self.calibrator = CalibratorMultiFile(pedestal_path, sn_list)
@@ -10,6 +10,8 @@ class WaveformCalibrator:
             from target_calib import Calibrator
             self.calibrator = Calibrator(pedestal_path)
         self.calibrated_wfs = None
+        self.n_pixels = n_pixels
+        self.n_samples = n_samples
 
     def __call__(self, waveforms, fci):
         if self.calibrated_wfs is None:
@@ -20,4 +22,6 @@ class WaveformCalibrator:
     @classmethod
     def from_tio_reader(cls, pedestal_path, tio_reader):
         sn_list = [tio_reader.get_sn(tm) for tm in range(tio_reader.n_modules)]
-        return cls(pedestal_path, sn_list)
+        n_pixels = tio_reader.n_pixels
+        n_samples = tio_reader.n_samples
+        return cls(pedestal_path, n_pixels, n_samples, sn_list)
