e3265be3a3a24b9226e8f4f552ceb6de5fab908e
watsonjj <jason.jw@live.co.uk>
2019-04-28 22:54:22 +0200

    Created TimeCalibrator

--- CHECLabPy/calib/__init__.py
+++ CHECLabPy/calib/__init__.py
@@ -5,3 +5,4 @@ from .pixel_masks import PixelMasks
 from . illumination_profile import IlluminationProfile
 from .amplitude_calibrator import AmplitudeCalibrator
 from .sipm_data_sheet import SiPMDataSheetConverter
+from .time_calibrator import TimeCalibrator
