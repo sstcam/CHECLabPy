0af71245a0d47e7b1f4668d3f7f1a03d62cd277a
watsonjj <jason.jw@live.co.uk>
2019-05-01 19:30:54 +0200

    Formatting

--- CHECLabPy/calib/__init__.py
+++ CHECLabPy/calib/__init__.py
@@ -2,7 +2,7 @@ from .data import get_calib_data
 from .waveform import WaveformCalibrator
 from .amplitude_matcher import AmplitudeMatcher
 from .pixel_masks import PixelMasks
-from . illumination_profile import IlluminationProfile
+from .illumination_profile import IlluminationProfile
 from .amplitude_calibrator import AmplitudeCalibrator
 from .sipm_data_sheet import SiPMDataSheetConverter
 from .time_calibrator import TimeCalibrator
