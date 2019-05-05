f0be67d71acb3e12629ab49c2180b30427b1a921
watsonjj <jason.jw@live.co.uk>
2019-05-05 14:18:13 +0200

    Updated sipm_data_sheet.py

--- CHECLabPy/calib/sipm_data_sheet.py
+++ CHECLabPy/calib/sipm_data_sheet.py
@@ -6,6 +6,9 @@ from scipy.interpolate import interp1d
 
 class SiPMDataSheetConverter:
     def __init__(self):
+        """
+        Convert between SiPM characteristics using the S12642 data sheet
+        """
         path = get_calib_data("sipm_data_sheet.csv")
         self.df = pd.read_csv(path)
 
@@ -26,3 +29,21 @@ class SiPMDataSheetConverter:
                     f = interp1d(x, self.df[column].values)
                     interpolated[column] = f(value)
                 return interpolated
+
+    def plot(self):
+        from matplotlib import pyplot as plt
+        x = self.df['vover'].values
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+        ln1 = ax1.plot(x, self.df['gain'], color='green', label="Gain")
+        ln2 = ax2.plot(x, self.df['pde'], color='blue', label="PDE")
+        ln3 = ax2.plot(x, self.df['opct'], color='red', label="OPCT")
+        ax1.set_xlabel("Vover (V)")
+        ax1.set_ylabel("Gain")
+        ax2.set_ylabel("PDE, OPCT")
+        ax1.set_ylim(0, 4e6)
+        ax2.set_ylim(0, 1)
+        lns = ln1 + ln2 + ln3
+        labs = [l.get_label() for l in lns]
+        ax1.legend(lns, labs, loc='best')
+        plt.show()
