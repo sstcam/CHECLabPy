Correct normalization

CHECLabPy/core/spectrum_fitter.py
@@ -240,6 +240,9 @@ class SpectrumFitter:
             self.parameters.lookup_typed,
             *self.fit_result_values.values()
         )
+        for i in range(self.n_illuminations):
+            hist_area = np.trapz(self.charge_hist_y[i], self.charge_hist_x)
+            fit_y[i] *= hist_area / np.trapz(fit_y[i], fit_x)
         return fit_x, fit_y
 
     @property
@@ -272,7 +275,7 @@ class SpectrumFitter:
         self.charge_hist_y_typed = typed.List()
         for i in range(self.n_illuminations):
             hist, edges = np.histogram(
-                charges[i], bins=self.n_bins, range=self.range, density=True
+                charges[i], bins=self.n_bins, range=self.range
             )
             between = (edges[1:] + edges[:-1]) / 2
 
@@ -281,30 +284,34 @@ class SpectrumFitter:
             self.charge_hist_y_typed.append(hist.astype(np.float32))
             self.charge_hist_edges = edges.astype(np.float32)
 
-        minimize_function = partial(
-            self._get_likelihood,
-            self.n_illuminations,
-            self.charge_hist_x,
-            self.charge_hist_y_typed,
-            self.parameters.lookup_typed
-        )
-
         m0 = iminuit.Minuit(
-            minimize_function, **self.parameters.minuit_kwargs,
-            print_level=0, pedantic=False, throw_nan=True,
+            self._minimize_function, **self.parameters.minuit_kwargs,
+            print_level=0, pedantic=False, throw_nan=True, errordef=1,
             forced_parameters=self.parameters.parameter_names
         )
         m0.migrad()
         self.fit_result_values = m0.values
 
-        # with warnings.catch_warnings():
-        #     warnings.simplefilter('ignore', HesseFailedWarning)
-        m0.hesse()
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', HesseFailedWarning)
+            m0.hesse()
         self.fit_result_errors = m0.errors
+
+    def _minimize_function(self, *parameter_values):
+        try:
+            return self._get_likelihood(
+                self.n_illuminations,
+                self.charge_hist_x,
+                self.charge_hist_y_typed,
+                self.parameters.lookup_typed,
+                *parameter_values,
+            )
+        except ZeroDivisionError:
+            return np.inf
 
     @staticmethod
     @abstractmethod
-    def _get_spectra(n_illuminations, data_x, data_y, lookup, *parameter_values):
+    def _get_spectra(n_illuminations, data_x, lookup, *parameter_values):
         """
         Abstract method to be defined by the SpectrumFitter subclass
 

CHECLabPy/core/tests/test_sprectrum_fitter.py
@@ -129,9 +129,9 @@ def test_spectrum_fitter():
     np.testing.assert_allclose(parameter_values['std'], std, rtol=1e-2)
 
     parameter_errors = fitter.fit_result_errors
-    assert parameter_errors['mean0'] < 0.1
-    assert parameter_errors['mean1'] < 0.1
-    assert parameter_errors['std'] < 0.1
+    assert parameter_errors['mean0'] < 0.01
+    assert parameter_errors['mean1'] < 0.01
+    assert parameter_errors['std'] < 0.01
 
     hist_x, hist_y, hist_edges = fitter.charge_histogram
     assert hist_x.size == fitter.n_bins

CHECLabPy/spectrum_fitters/gentile.py
@@ -15,16 +15,15 @@ class SiPMGentileFitter(SpectrumFitter):
         super().__init__(n_illuminations, config_path)
 
         self.parameters = SpectrumParameterCollection([
-            SpectrumParameter("norm", 1, (0.5, 1.5)),
             SpectrumParameter("eped", 0, (-10, 10)),
-            SpectrumParameter("eped_sigma", 9, (2, 20)),
-            SpectrumParameter("spe", 25, (15, 40)),
-            SpectrumParameter("spe_sigma", 2, (1, 20)),
-            SpectrumParameter("opct", 0.4, (0.01, 0.8)),
-            SpectrumParameter("lambda_", 0.7, (0.001, 3), multi=True),
+            SpectrumParameter("eped_sigma", 9, (0, 20)),
+            SpectrumParameter("spe", 25, (0, 40)),
+            SpectrumParameter("spe_sigma", 2, (0, 20)),
+            SpectrumParameter("opct", 0.4, (0, 1)),
+            SpectrumParameter("lambda_", 0.7, (0, 6), multi=True),
         ], n_illuminations, config_path)
         self.n_bins = 100
-        self.range = (-40, 150)
+        self.range = (-30, 200)
 
     @staticmethod
     @njit(fastmath=True)
@@ -41,6 +40,7 @@ class SiPMGentileFitter(SpectrumFitter):
         likelihood = 0
         for i in range(n_illuminations):
             spectrum = calculate_spectrum(data_x, lookup[i], *parameter_values)
+            spectrum *= np.trapz(data_y[i], data_x) / np.trapz(spectrum, data_x)
             likelihood += np.nansum(-2 * poisson_logpmf(data_y[i], spectrum))
         return likelihood
 
@@ -49,7 +49,6 @@ class SiPMGentileFitter(SpectrumFitter):
 def calculate_spectrum(data_x, lookup, *parameter_values):
     return sipm_gentile_spe(
         x=data_x,
-        norm=parameter_values[lookup["norm"]],
         eped=parameter_values[lookup["eped"]],
         eped_sigma=parameter_values[lookup["eped_sigma"]],
         spe=parameter_values[lookup["spe"]],
@@ -60,7 +59,7 @@ def calculate_spectrum(data_x, lookup, *parameter_values):
 
 
 @njit(fastmath=True)
-def sipm_gentile_spe(x, norm, eped, eped_sigma, spe, spe_sigma, opct, lambda_):
+def sipm_gentile_spe(x, eped, eped_sigma, spe, spe_sigma, opct, lambda_):
     """
     Fit for the SPE spectrum of a SiPM
 
@@ -84,14 +83,13 @@ def sipm_gentile_spe(x, norm, eped, eped_sigma, spe, spe_sigma, opct, lambda_):
 
     Returns
     -------
-    signal : ndarray
-        The y values of the total signal.
+    spectrum : ndarray
+        The y values of the total spectrum.
     """
     # Obtain pedestal peak
     p_ped = exp(-lambda_)
-    ped_signal = norm * p_ped * normal_pdf(x, eped, eped_sigma)
+    spectrum = p_ped * normal_pdf(x, eped, eped_sigma)
 
-    pe_signal = np.zeros(x.size)
     pk_max = 0
 
     # Loop over the possible total number of cells fired
@@ -119,6 +117,6 @@ def sipm_gentile_spe(x, norm, eped, eped_sigma, spe, spe_sigma, opct, lambda_):
         pe_sigma = sqrt(k * spe_sigma ** 2 + eped_sigma ** 2)
 
         # Evaluate probability at each value of x
-        pe_signal += norm * pk * normal_pdf(x, eped + k * spe, pe_sigma)
+        spectrum += pk * normal_pdf(x, eped + k * spe, pe_sigma)
 
-    return ped_signal + pe_signal
+    return spectrum

CHECLabPy/spectrum_fitters/mapm.py
@@ -15,10 +15,10 @@ class MAPMFitter(SpectrumFitter):
 
         self.parameters = SpectrumParameterCollection([
             SpectrumParameter("eped", 0, (-10, 10)),
-            SpectrumParameter("eped_sigma", 9, (2, 20)),
-            SpectrumParameter("spe", 25, (5, 30)),
-            SpectrumParameter("spe_sigma", 2, (1, 20)),
-            SpectrumParameter("lambda_", 0.7, (0.001, 3), multi=True),
+            SpectrumParameter("eped_sigma", 9, (0, 20)),
+            SpectrumParameter("spe", 25, (0, 40)),
+            SpectrumParameter("spe_sigma", 2, (0, 20)),
+            SpectrumParameter("lambda_", 0.7, (0, 6), multi=True),
         ], n_illuminations, config_path)
         self.n_bins = 100
         self.range = (-40, 150)
@@ -38,6 +38,7 @@ class MAPMFitter(SpectrumFitter):
         likelihood = 0
         for i in range(n_illuminations):
             spectrum = calculate_spectrum(data_x, lookup[i], *parameter_values)
+            spectrum *= np.trapz(data_y[i], data_x) / np.trapz(spectrum, data_x)
             likelihood += np.nansum(-2 * poisson_logpmf(data_y[i], spectrum))
         return likelihood
 